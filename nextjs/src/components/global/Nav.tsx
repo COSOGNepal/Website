e8d89d7c2c35b366f0c94b5a8b83831030d013d8
sarojregmi200 <sarojregmi.official@gmail.com>
2023-08-29 09:56:03 +0545

    Update: navigation markup complete

--- nextjs/src/components/global/Nav.tsx
+++ nextjs/src/components/global/Nav.tsx
@@ -2,7 +2,33 @@ import React from 'react'
 
 function Nav() {
   return (
-    <div className='h-100 bg-red'>Nav</div>
+    <div className='px-8 py-4 flex items-center justify-between w-1/1'>
+      <div className='font-bold text-2xl'>
+        Logo
+      </div>
+    <div className='navContents flex align-middle'>
+     <div className='text-white text-base'>
+          Home
+    </div>
+    <div className='text-white text-base mx-5'>
+          Blog
+    </div>
+    <div className='text-white text-base'>
+          Contact
+    </div>
+    </div>
+
+<div className='flex align-middle'>
+    <div className='h-10 leading-10 bg-btn-sec-red text-center rounded-lg w-44'>
+          Login
+    </div>
+    <div className='h-10 leading-10 bg-btn-pri-red text-center rounded-lg w-44 ml-5'>
+          Register
+    </div>
+    </div>
+
+
+    </div>
   )
 }
 
--- nextjs/tailwind.config.ts
+++ nextjs/tailwind.config.ts
@@ -6,5 +6,11 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  theme:{
+    colors:{
+      "btn-sec-red": "#462935",
+      "btn-pri-red": "#E33F2BBD",
+    } 
+  }
 }
 export default config
