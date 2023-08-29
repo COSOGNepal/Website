f9941f5a0f1739d852dbad0cae551d42a6927aff
sarojregmi200 <sarojregmi.official@gmail.com>
2023-08-29 12:04:18 +0545

    Update: added the quote text

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -5,7 +5,12 @@ export default function Home() {
   return(
     <>
       <Nav />
-      <div className="bg-main-gradient h-screen w-full ">
+      <div className="bg-main-gradient h-screen w-full flex items-center justify-center">
+        <h1 className="w-full text-center text-6xl font-extrabold md:text-4xl ">
+          Code to inspire, Code to change
+          <br />
+          Code for Social Good
+        </h1>
       </div> 
     </>
   )
