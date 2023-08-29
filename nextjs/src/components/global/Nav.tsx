8acb849559f66ef9257bdc2526b0c6b4cabd15b3
sarojregmi200 <sarojregmi.official@gmail.com>
2023-08-29 17:35:34 +0545

    Update: changed the auth button to a section called connect.

--- nextjs/src/components/global/Nav.tsx
+++ nextjs/src/components/global/Nav.tsx
@@ -8,13 +8,13 @@ function Nav(): React.JSX.Element {
   const router = useRouter();
 
   return (
-    <div className='fixed top-0 left-0 px-8 py-4 flex items-center justify-between w-full'>
+    <div className='fixed top-0 left-0 px-8 py-4 flex items-center  w-full'>
       <Link href={"/"}> 
         <div className='font-bold text-2xl'>
           Logo
         </div>
       </Link> 
-      <div className='navContents flex items-center'>
+      <div className='navContents flex items-center flex-1 justify-center'>
         <Link href={'/home'}>
           <div className='text-white text-base opacity-70 hover:opacity-100'>
             Home
@@ -31,7 +31,7 @@ function Nav(): React.JSX.Element {
       </div>
 
       <div className='flex align-middle'>
-        <div className='h-10 leading-10 bg-btn-sec-red text-center rounded-lg w-44 cursor-pointer opacity-50 hover:opacity-70' onClick={()=>{router.push("login")}}>
+        <div className='h-10 leading-10 bg-btn-sec-red text-center rounded-lg w-44 cursor-pointer opacity-50 hover:opacity-70' onClick={()=>{router.push("/authenticate")}}>
           Connect
         </div>
         
