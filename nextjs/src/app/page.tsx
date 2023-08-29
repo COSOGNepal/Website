650516d41c97860e213effd769261e9cd4cdea90
sarojregmi200 <sarojregmi.official@gmail.com>
2023-08-29 11:08:54 +0545

    Update: added the navigation to the links

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -4,5 +4,6 @@ import { Nav } from "@/components/global";
 export default function Home() {
   return <div className="bg-blue">
       <Nav />
+      
   </div> 
 }
--- nextjs/src/components/global/Nav.tsx
+++ nextjs/src/components/global/Nav.tsx
@@ -1,33 +1,45 @@
+"use client"
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 function Nav() {
+  
+  const router = useRouter();
+
   return (
-    <div className='px-8 py-4 flex items-center justify-between w-1/1'>
-      <div className='font-bold text-2xl'>
-        Logo
-      </div>
-    <div className='navContents flex align-middle'>
-     <div className='text-white text-base'>
-          Home
-    </div>
-    <div className='text-white text-base mx-5'>
-          Blog
-    </div>
-    <div className='text-white text-base'>
+
+    <div className='fixed top-0 left-0 px-8 py-4 flex items-center justify-between w-full'>
+      <Link href={"/"}> 
+        <div className='font-bold text-2xl'>
+          Logo
+        </div>
+      </Link> 
+      <div className='navContents flex align-middle'>
+        <Link href={'/home'}>
+          <div className='text-white text-base'>
+            Home
+          </div>
+        </Link> 
+        <Link href={"/blog"}>
+          <div className='text-white text-base mx-5'>
+            Blog
+          </div> 
+        </Link> 
+        <Link href={"/contact"}> <div className='text-white text-base'>
           Contact
-    </div>
-    </div>
+        </div></Link> 
+      </div>
 
-<div className='flex align-middle'>
-    <div className='h-10 leading-10 bg-btn-sec-red text-center rounded-lg w-44'>
+      <div className='flex align-middle'>
+        <div className='h-10 leading-10 bg-btn-sec-red text-center rounded-lg w-44 cursor-pointer' onClick={()=>{router.push("login")}}>
+
           Login
-    </div>
-    <div className='h-10 leading-10 bg-btn-pri-red text-center rounded-lg w-44 ml-5'>
+        </div>
+        <div className='h-10 leading-10 bg-btn-pri-red text-center rounded-lg w-44 ml-5 cursor-pointer' onClick={()=>{router.push("register")}}>
           Register
-    </div>
-    </div>
-
-
+        </div>
+      </div>
     </div>
   )
 }
