c5738568b4baa3ebdb4d057d4d2400c64f7b53ed
sarojregmi200 <sarojregmi.official@gmail.com>
2023-09-03 14:26:49 +0545

    Update: moved the colors in the extended theme

--- nextjs/tailwind.config.ts
+++ nextjs/tailwind.config.ts
@@ -7,18 +7,17 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme:{
-    colors:{
-      "btn-pri": "#6384D1",
-      "btn-sec": "#6384D12A",
-      "btn-sec-hover": "#6384D14A",
-            
-    },
     extend:{
     backgroundImage:{
       "main-gradient": "linear-gradient(223deg, rgba(244, 68, 46, 0.20) 0%, rgba(3, 2, 34, 0.20) 100%), radial-gradient(139.09% 130.87% at 23.16% -57.28%, #959577 0%, #020122 100%)"
       }
+    },
+    colors:{
+      "btn-pri": "#6384D1",
+      "btn-sec": "#6384D12A",
+      "btn-sec-hover": "#6384D14A",
     }
   },
-
+  plugins:[]
 }
 export default config
