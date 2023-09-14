b56407bd6614d394aa9aa371fec28681af9290ad
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-09-14 23:09:38 +0545

    footer section complete

---
+++ nextjs/src/components/global/SocialMediaIcons.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+
+export const Facebook = () => {
+  return <Image src={"/icons/Facebook.svg"} width={20} height={20} alt="fb" />;
+};
+export const GitHub = () => {
+  return <Image src={"/icons/GitHub.svg"} width={20} height={20} alt="fb" />;
+};
+export const Instagram = () => {
+  return <Image src={"/icons/Instagram.svg"} width={20} height={20} alt="fb" />;
+};
+export const Linkedin = () => {
+  return <Image src={"/icons/Linkedin.svg"} width={20} height={20} alt="fb" />;
+};
+export const Twitter = () => {
+  return <Image src={"/icons/Twitter.svg"} width={20} height={20} alt="fb" />;
+};
