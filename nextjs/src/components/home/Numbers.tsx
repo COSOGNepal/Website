b3d3d580a3db2175b3984949395c295393d249a5
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-09-18 22:28:23 +0545

    numbers animation

--- nextjs/src/components/home/Numbers.tsx
+++ nextjs/src/components/home/Numbers.tsx
@@ -1,54 +1,62 @@
-import React, { useEffect, useRef } from "react";
-import increasedNumber from "./../../utils/increaseNumbers";
+import React, { useEffect, ref, useRef } from "react";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  animate,
+  useInView,
+} from "framer-motion";
 
 const Numbers = () => {
-  const NumbersRef = useRef() || null;
-  let callback = (entries, observer) => {
-    entries.forEach((entry) => {
-      console.log(entry);
-      // Each entry describes an intersection change for one observed
-      // target element:
-      //   entry.boundingClientRect
-      //   entry.intersectionRatio
-      //   entry.intersectionRect
-      //   entry.isIntersecting
-      //   entry.rootBounds
-      //   entry.target
-      //   entry.time
-    });
+  const membersCount = useMotionValue(0);
+  const projectsCount = useMotionValue(0);
+  const eventsCount = useMotionValue(0);
+  const members = useTransform(membersCount, Math.round);
+  const projects = useTransform(projectsCount, Math.round);
+  const events = useTransform(eventsCount, Math.round);
+  const NumbersRef = useRef(null);
+  const isInView = useInView(NumbersRef);
+  window.onscroll = () => {
+    console.log(isInView);
   };
   useEffect(() => {
-    let options = {
-      root: document.querySelector("body"),
-      rootMargin: "20px",
-      threshold: 1.0,
-    };
+    if (isInView) {
+      const animation1 = animate(membersCount, 20, { duration: 4 });
+      const animation2 = animate(projectsCount, 10, { duration: 2 });
+      const animation3 = animate(eventsCount, 15, { duration: 3 });
+      //animation duration kinda sucks
+    }
+  }, [isInView]);
 
-    let observer = new IntersectionObserver(callback, options);
-    observer.observe(NumbersRef.current);
-    console.log(NumbersRef.current);
-  }, []);
   return (
     <main
       ref={NumbersRef}
       className="grid grid-cols-1 gap-y-6 md:grid-cols-3 bg-dark-pri py-12 h-fit md:h-64 items-center justify-items-center"
     >
       <div>
-        <h2 className="text-white text-6xl text-center font-extrabold">
-          {increasedNumber(30)}+
-        </h2>
+        <div className="text-white flex text-6xl text-center font-extrabold">
+          {" "}
+          <motion.h1>{projects}</motion.h1>+
+        </div>
+
         <p className="text-text-grey-sec text-lg font-normal">
           Open source projects
         </p>
       </div>
       <div>
-        <h2 className="text-white text-6xl text-center font-extrabold">10+</h2>
+        <div className="text-white flex text-6xl text-center font-extrabold">
+          {" "}
+          <motion.h1>{members}</motion.h1>+
+        </div>
         <p className="text-text-grey-sec text-lg font-normal">
-          Community Members{" "}
+          Community Leaders{" "}
         </p>
       </div>
       <div>
-        <h2 className="text-white text-6xl text-center font-extrabold">10+</h2>
+        <div className="text-white flex text-6xl text-center font-extrabold">
+          {" "}
+          <motion.h1>{events}</motion.h1>+
+        </div>{" "}
         <p className="text-text-grey-sec text-lg font-normal">
           Social Good Events{" "}
         </p>
