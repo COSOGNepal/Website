5afbb47ea2534f149f077eba0c3f7ebbb9d8dd7c
sarojregmi200 <sarojregmi.official@gmail.com>
2023-09-20 06:04:25 +0545

    Using external css file for more complex anims and designs.

--- nextjs/src/components/home/Project.tsx
+++ nextjs/src/components/home/Project.tsx
@@ -1,25 +1,35 @@
 import React from "react";
 import { Button } from "../global";
 import Link from "next/link";
+import "./style/project.css";
 
 export default function Project({ data, id }: { data: string, id: string }) {
     const seeCodeAction = () => { };
     const seeInAction = () => { };
     return (
-        <Link href={`/projects/${id}`} className="mt-[50px] cursor-pointer w-[60%] min-h-[500px] grid grid-cols-2 border-gray-300 border-[1px] overflow-hidden rounded-md max-md:grid-cols-1 max-md:w-full max-md:grid-rows-prjt-min-brk">
-            <div className="bg-no-repeat bg-cover relative group" style={{ backgroundImage: `url(/testImg.jpg)` }}>
-                <div className="opacity-0 group-hover:opacity-100 bg-overlay absolute top-0 left-0 flex justify-center items-center h-full w-full">
+        <Link
+            href={`/projects/${id}`}
+            className="main_container mt-[50px] cursor-pointer w-[60%] min-h-[500px] grid grid-cols-2 border-gray-300 border-[1px] overflow-hidden rounded-md max-md:grid-cols-1 max-md:w-full max-md:grid-rows-prjt-min-brk "
+
+
+        >
+            <div
+                className="image_section bg-no-repeat bg-cover relative group"
+                style={{ backgroundImage: `url(/testImg.jpg)` }}
+            >
+                <div
+                    className="image_section_overlay opacity-0 group-hover:opacity-100 bg-overlay absolute top-0 left-0 flex justify-center items-center h-full w-full">
                     <LinkIcon />
                 </div>
             </div>
-            <div className="p-[50px]" >
-                <div>
+            <div className="p-[50px] text_section" >
+                <div className="text_section_fp1">
                     <h1 className="text-main-txt font-bold text-sub-heading">NFT Marketplace</h1>
                     <p className="text-sec-txt font-medium text-small">
                         A great place to buy NFT and get rich as soon as possible it is just amazing project you will not get it but anyways keep looking press the links below, if not go to hell and don’t contact us ever again.
                     </p>
                 </div>
-                <div className="mt-[50px]">
+                <div className="mt-[50px] text_section_fp2">
                     <h1 className="text-main-txt font-bold text-sub-heading">Main features:</h1>
                     <p className="text-sec-txt font-medium text-small">
                         1. It is an amazing site. <br />
@@ -28,7 +38,7 @@ export default function Project({ data, id }: { data: string, id: string }) {
                         4. It uses css as the database so its very good.
                     </p>
                 </div>
-                <div className="flex justify-center mt-[50px]">
+                <div className="flex justify-center mt-[50px] action_buttons">
                     <Button text="See Code" styles="bg-sec-txt" onclick={seeCodeAction} />
                     <Button text="See in action" styles="ml-[50px] bg-dark-pri text-white" onclick={seeInAction} />
                 </div>
