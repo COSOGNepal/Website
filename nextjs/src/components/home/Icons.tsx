2b22dc9b36f10e9aa61260ab3987e83e03a8cac7
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-10-15 15:46:56 +0545

    logo opacity fixes

--- nextjs/src/components/home/Icons.tsx
+++ nextjs/src/components/home/Icons.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 
 import {
   Html,
@@ -20,50 +20,53 @@ import {
 import { getRandomOpacity } from "@/utils/randomizer";
 
 const Icons = () => {
+  useEffect(() => {
+    console.log(getRandomOpacity());
+  });
   return (
     <div className="w-full h-full absolute top-0 left-0 pointer-events-none -z-5 overflow-hidden">
       <div className="relative w-full h-full  pointer-events-none ">
         <Html
-          style={`opacity-${getRandomOpacity()} absolute animate-move-random-1`}
+          style={`opacity-${getRandomOpacity().toString()} absolute animate-move-random-1`}
         />
         <Bash
-          style={`opacity-${getRandomOpacity()} absolute animate-move-random-2`}
+          style={`opacity-${getRandomOpacity().toString()} absolute animate-move-random-2`}
         />
         <BashM
-          style={`opacity-${getRandomOpacity()} absolute animate-move-random-3`}
+          style={`opacity-${getRandomOpacity().toString()} absolute animate-move-random-3`}
         />
         <Css
-          style={`opacity-${getRandomOpacity()} absolute animate-move-random-4`}
+          style={`opacity-${getRandomOpacity().toString()} absolute animate-move-random-4`}
         />
         <Golang
-          style={`opacity-${getRandomOpacity()} absolute animate-move-random-5`}
+          style={`opacity-${getRandomOpacity().toString()} absolute animate-move-random-5`}
         />
         <Html
-          style={`opacity-${getRandomOpacity()} absolute animate-move-random-6`}
+          style={`opacity-${getRandomOpacity().toString()} absolute animate-move-random-6`}
         />
         <Java
-          style={`opacity-${getRandomOpacity()} absolute animate-move-random-7`}
+          style={`opacity-${getRandomOpacity().toString()} absolute animate-move-random-7`}
         />
         <Js
-          style={`opacity-${getRandomOpacity()} absolute animate-move-random-8`}
+          style={`opacity-${getRandomOpacity().toString()} absolute animate-move-random-8`}
         />
         <Kotlin
-          style={`opacity-${getRandomOpacity()} absolute animate-move-random-9`}
+          style={`opacity-${getRandomOpacity().toString()} absolute animate-move-random-9`}
         />
         <Python
-          style={`opacity-${getRandomOpacity()} absolute animate-move-random-10`}
+          style={`opacity-${getRandomOpacity().toString()} absolute animate-move-random-10`}
         />
         <Rails
-          style={`opacity-${getRandomOpacity()} absolute animate-move-random-11`}
+          style={`opacity-${getRandomOpacity().toString()} absolute animate-move-random-11`}
         />
         <ReactIcon
-          style={`opacity-${getRandomOpacity()} absolute animate-move-random-12`}
+          style={`opacity-${getRandomOpacity().toString()} absolute animate-move-random-12`}
         />
         <Ruby
-          style={`opacity-${getRandomOpacity()} absolute animate-move-random-13`}
+          style={`opacity-${getRandomOpacity().toString()} absolute animate-move-random-13`}
         />
         <Rust
-          style={`opacity-${getRandomOpacity()} absolute animate-move-random-14`}
+          style={`opacity-${getRandomOpacity().toString()} absolute animate-move-random-14`}
         />
       </div>
     </div>
