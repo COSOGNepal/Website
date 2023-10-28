1165589c6ff7abd53eaf04683653cd2b95a9dc1e
Aashish Panthi <aashishpanthi11@gmail.com>
2023-10-28 16:16:14 +0545

    solve the errors

--- nextjs/src/app/index/VideoDemo.tsx
+++ nextjs/src/app/index/VideoDemo.tsx
@@ -1,12 +1,13 @@
 import React, { useRef, useState } from "react";
 
 type cursorModeTypes = "play" | "pause" | "hidden";
+
 function VideoDemo() {
   const videoRef = useRef<HTMLVideoElement>(null);
   const cursorRef = useRef<HTMLDivElement>(null);
   const [cursorMode, setCursor] = useState<cursorModeTypes>("play");
 
-  const handleMouseMove = (e: MouseEvent) => {
+  const handleMouseMove = (e: React.MouseEvent<HTMLVideoElement, MouseEvent>) => {
     const clientX = e.clientX;
     const clientY = e.clientY;
 
@@ -15,6 +16,7 @@ function VideoDemo() {
     cursorRef.current.style.top = `${clientY}px`;
     cursorRef.current.style.left = `${clientX}px`;
   };
+
   const handleMouseLeave = () => {
     setCursor("hidden");
   };
