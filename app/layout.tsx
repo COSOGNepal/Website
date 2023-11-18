5fdd2524df1a7fac112c832fb9329df55095af5d
Aashish Panthi <aashishpanthi11@gmail.com>
2023-11-18 09:44:30 +0545

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import "./globals.css";
 import Footer from "./_components/Footer";
 
 export const metadata: Metadata = {
-  title: "COSOG Nepal",
-  description: "Code to make change, Code for social good.",
+  title: "Coding for Social Good (CoSoG) Nepal",
+  description: "Cosog Nepal (Coding for Social Good) is a non-profit organization working to promote Computer Science Education and Initiate CS Clubs in Sec/High Schools in Nepal and facilitating charitable coding initiatives to aid in Nepal's digitalization.",
 };
 
 export default function RootLayout({
@@ -15,6 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <meta property="og:image" content="/assets/logo.png" />
+      <meta property="og:image:type" content="image/png" />
+      <meta property="og:image:width" content="386" />
+      <meta property="og:image:height" content="386" />
       <body className="flex flex-col justify-between">
         <Navbar />
         {children}
