272ed38800ca63cc3ad8b22188ebb0e0a38c5a38
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-23 08:42:45 +0545

    About section markup complete

---
+++ app/about/page.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import Image from "next/image";
+import type { TdescriptorData } from "./type";
+export default function AboutPage() {
+
+    return (
+        <div className="main_container">
+            <Image src="/images/banner" width={1230} height={470} alt="banner_about_section" className="banner" />
+            <div className="description">
+                <Descriptor title="Who are we?" description="Cosog Nepal (Coding for Social Good) is a non-profit organization working to promote Computer Science Education and Initiate CS Clubs in Sec/High Schools in Nepal and facilitating charitable coding initiatives to aid in Nepal's digitalization." />
+                <Descriptor title="Our story" description="Force leg push. Change recognize cultural natural. Material life someone hand attorney hot build. Bring pay upon light company your car. Success none throw. Both kitchen past deal eye east." />
+                <Descriptor title="Our Mission" description="Population life level half. Investment program modern beat machine black. Last six there street enter agent. Begin technology PM wife million there movement matter."
+                    list={["Personal probably wait interest worker its. Member others throw scene two mission camera.",
+                        "Personal probably wait interest worker its. Member others throw scene two mission camera.",
+                        "Personal probably wait interest worker its. Member others throw scene two mission camera."
+                    ]}
+                />
+                <Descriptor title="Our Impact" description="Population life level half. Investment program modern beat machine black. Last six there street enter agent. Begin technology PM wife million there movement matter."
+                    list={["Personal probably wait interest worker its. Member others throw scene two mission camera.",
+                        "Personal probably wait interest worker its. Member others throw scene two mission camera.",
+                        "Personal probably wait interest worker its. Member others throw scene two mission camera."
+                    ]}
+                />
+            </div>
+        </div>
+    )
+
+}
+
+
+function Descriptor({ title, description, list = [] }: TdescriptorData) {
+
+    return (
+        <div className="descriptor">
+            <div className="title">
+                {title}
+            </div>
+            <div className="descriptin">
+                {description}
+            </div>
+            <div className="list_con">
+                {
+                    list?.map((li, index) => {
+                        return <li key={index}>{li}</li>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
---
+++ app/about/type.ts
@@ -0,0 +1,5 @@
+export type TdescriptorData = {
+    title: string,
+    description: string,
+    list?: string[]
+}
