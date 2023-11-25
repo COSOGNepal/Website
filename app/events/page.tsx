8b1d4ab1fc1f5cb189e18e47b38d261a2975b8fb
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-26 00:21:29 +0545

    Slider position layout started.

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -2,8 +2,17 @@ import Event from "./Event";
 
 export default function EventsPage() {
     return (
-        <section className="mt-section px-block">
-            <Event />
+        <section className="mt-section px-block flex justify-between max-w-[1400px] m-auto">
+            <div className="events space-y-block">
+                <Event />
+                <Event />
+                <Event />
+            </div>
+            <div className="slider max-w-[300px] min-w-[250px] min-h-[500px] max-h-screen bg-black-light">
+
+
+            </div>
+
         </section>
     )
 
