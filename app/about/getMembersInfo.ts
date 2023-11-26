08e747570cfa6e7fd9a76d6f94674a170d53151f
Anish949 <74363280+Anish949@users.noreply.github.com>
2023-11-26 11:51:45 +0545

    navbar auto close

--- app/about/getMembersInfo.ts
+++ app/about/getMembersInfo.ts
@@ -1,181 +1,186 @@
 import type { Tmember } from "./type";
 
 export default async function getMembers(): Promise<Tmember[]> {
-    const data: Tmember[] = [
-        {
-            "name": "Bibek Bhandari",
-            "email": "bhandareebivek@gmail.com",
-            "fb_link": "",
-            "insta_link": "https://www.instagram.com/vandari_7/",
-            "linkedin_link": "https://www.linkedin.com/in/bibekbha/",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Precident"
-        },
-        {
-            "name": "Aashish Panthi",
-            "email": "aashishpanthi11@gmail.com",
-            "fb_link": "https://www.facebook.com/aashishpanthi11",
-            "insta_link": "https://www.instagram.com/aashishpanthi11/",
-            "linkedin_link": "https://www.linkedin.com/in/aashishpanthi/",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Vice Precident",
-        },
-        {
-            "name": "Sushil Bhattarai",
-            "email": "bhattaraisushil005@gmail.com",
-            "fb_link": "https://www.facebook.com/sushilbhattaraiofficial",
-            "insta_link": "instagram.com/sushil_bhattarai45",
-            "linkedin_link": "https://www.linkedin.com/in/sushilbhattarai45",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Secratory"
-        },
-        {
-            "name": "Pujan Pokharel",
-            "email": "info.pujanpokharel@gmail.com",
-            "fb_link": "https://www.facebook.com/OfficialPujanPokharel",
-            "insta_link": "https://www.instagram.com/pujanpokharelofficial/",
-            "linkedin_link": "https://www.linkedin.com/in/pujanpokharelofficial/",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Treasurer"
-        },
-        {
-            "name": "Saroj Regmi",
-            "email": "sarojregmi.official@gmail.com",
-            "fb_link": "https://www.facebook.com/sarojregmi200",
-            "insta_link": "https://www.instagram.com/sarojregmi200",
-            "linkedin_link": "https://www.linkedin.com/in/sarojregmi200",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "It Head",
-        },
-        {
-            "name": "Sanskar Lamsal",
-            "email": "sskrlmsl@gmail.com",
-            "fb_link": "https://www.facebook.com/Sanskar.lmsl",
-            "insta_link": "https://instagram.com/ssanskaar_?igshid=OGQ5ZDc2ODk2ZA==",
-            "linkedin_link": "https://www.linkedin.com/in/sanskar-lamsal-b03a4b266",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Program Manager"
-        },
-        {
-            "name": "Adonis Sharma",
-            "email": "adonissharma111@gmail.com",
-            "fb_link": "https://www.facebook.com/profile.php?id=100092346487256",
-            "insta_link": "",
-            "linkedin_link": "https://www.linkedin.com/in/adonis-sharma-1083b91b1",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Community Leader"
-        },
-        {
-            "name": "Dijesh Shrestha",
-            "email": "sresthadijesh43@gmail.com",
-            "fb_link": "facebook.com/djdjesh",
-            "insta_link": "Instagram.com/djsh__14",
-            "linkedin_link": "https://www.linkedin.com/in/dijesh-shrestha-81985627b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Community Leader"
-        },
-        {
-            "name": "Anish Raj Pandey",
-            "email": "techzzoid@gmail.com",
-            "fb_link": "https://www.facebook.com/anishrajpandey0",
-            "insta_link": "https://www.instagram.com/pandeyanishraj/",
-            "linkedin_link": "https://www.linkedin.com/in/anish-raj-pandey-b13470258/",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Community Leader"
-        },
-        {
-            "name": "Aditya Bikram Thakur",
-            "email": "adthakur08@gmail.com",
-            "fb_link": "https://www.facebook.com/tankaprasad.budha.1",
-            "insta_link": "https://www.instagram.com/sirelvinn/",
-            "linkedin_link": "https://www.linkedin.com/in/aditya-bikram-thakur-a76501266/",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Community Leader"
-        },
-        {
-            "name": "Aadarsh Bhagat",
-            "email": "aadarshbhagat1112@gmail.com",
-            "fb_link": "https://www.facebook.com/AadarshaBhagataadarsh",
-            "insta_link": "https://www.instagram.com/vagaat/",
-            "linkedin_link": "https://www.linkedin.com/in/aadarsh-bhagat-a55343288/",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Community Leader"
-        },
-        {
-            "name": "Avipsha Dawadi",
-            "email": "dawadiavipsha01@gmail.com",
-            "fb_link": "",
-            "insta_link": "",
-            "linkedin_link": "https://www.linkedin.com/in/avipsha-dawadi-b3774327a?original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=np",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Community Leader"
-        },
-        {
-            "name": "Zenitha Chalise",
-            "email": "zenithachalise@gmail.com",
-            "fb_link": "https://www.facebook.com/zen.itha.9/",
-            "insta_link": "https://www.instagram.com/zenjtha/",
-            "linkedin_link": "https://www.linkedin.com/in/zenitha-chalise-0610b3279/",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Community Leader"
-        },
-        {
-            "name": "Abhinav Acharya",
-            "email": "abhinavacharya486@gmail.com",
-            "fb_link": "https://www.facebook.com/abhinav.acharya7s",
-            "insta_link": "https://www.instagram.com/abhinav.bat/",
-            "linkedin_link": "",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Community Leader"
-        },
+  const data: Tmember[] = [
+    {
+      name: "Bibek Bhandari",
+      email: "bhandareebivek@gmail.com",
+      fb_link: "",
+      insta_link: "https://www.instagram.com/vandari_7/",
+      linkedin_link: "https://www.linkedin.com/in/bibekbha/",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Precident",
+    },
+    {
+      name: "Aashish Panthi",
+      email: "aashishpanthi11@gmail.com",
+      fb_link: "https://www.facebook.com/aashishpanthi11",
+      insta_link: "https://www.instagram.com/aashishpanthi11/",
+      linkedin_link: "https://www.linkedin.com/in/aashishpanthi/",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Vice Precident",
+    },
+    {
+      name: "Sushil Bhattarai",
+      email: "bhattaraisushil005@gmail.com",
+      fb_link: "https://www.facebook.com/sushilbhattaraiofficial",
+      insta_link: "instagram.com/sushil_bhattarai45",
+      linkedin_link: "https://www.linkedin.com/in/sushilbhattarai45",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Secratory",
+    },
+    {
+      name: "Pujan Pokharel",
+      email: "info.pujanpokharel@gmail.com",
+      fb_link: "https://www.facebook.com/OfficialPujanPokharel",
+      insta_link: "https://www.instagram.com/pujanpokharelofficial/",
+      linkedin_link: "https://www.linkedin.com/in/pujanpokharelofficial/",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Treasurer",
+    },
+    {
+      name: "Saroj Regmi",
+      email: "sarojregmi.official@gmail.com",
+      fb_link: "https://www.facebook.com/sarojregmi200",
+      insta_link: "https://www.instagram.com/sarojregmi200",
+      linkedin_link: "https://www.linkedin.com/in/sarojregmi200",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "It Head",
+    },
+    {
+      name: "Sanskar Lamsal",
+      email: "sskrlmsl@gmail.com",
+      fb_link: "https://www.facebook.com/Sanskar.lmsl",
+      insta_link: "https://instagram.com/ssanskaar_?igshid=OGQ5ZDc2ODk2ZA==",
+      linkedin_link: "https://www.linkedin.com/in/sanskar-lamsal-b03a4b266",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Program Manager",
+    },
+    {
+      name: "Adonis Sharma",
+      email: "adonissharma111@gmail.com",
+      fb_link: "https://www.facebook.com/profile.php?id=100092346487256",
+      insta_link: "",
+      linkedin_link: "https://www.linkedin.com/in/adonis-sharma-1083b91b1",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Community Leader",
+    },
+    {
+      name: "Dijesh Shrestha",
+      email: "sresthadijesh43@gmail.com",
+      fb_link: "facebook.com/djdjesh",
+      insta_link: "Instagram.com/djsh__14",
+      linkedin_link:
+        "https://www.linkedin.com/in/dijesh-shrestha-81985627b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Community Leader",
+    },
+    {
+      name: "Anish Raj Pandey",
+      email: "anishpandey021@gmail.com",
+      fb_link: "https://www.facebook.com/anishrajpandey0",
+      insta_link: "https://www.instagram.com/pandeyanishraj/",
+      linkedin_link: "https://www.linkedin.com/in/anish-raj-pandey-b13470258/",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Community Leader",
+    },
+    {
+      name: "Aditya Bikram Thakur",
+      email: "adthakur08@gmail.com",
+      fb_link: "https://www.facebook.com/tankaprasad.budha.1",
+      insta_link: "https://www.instagram.com/sirelvinn/",
+      linkedin_link:
+        "https://www.linkedin.com/in/aditya-bikram-thakur-a76501266/",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Community Leader",
+    },
+    {
+      name: "Aadarsh Bhagat",
+      email: "aadarshbhagat1112@gmail.com",
+      fb_link: "https://www.facebook.com/AadarshaBhagataadarsh",
+      insta_link: "https://www.instagram.com/vagaat/",
+      linkedin_link: "https://www.linkedin.com/in/aadarsh-bhagat-a55343288/",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Community Leader",
+    },
+    {
+      name: "Avipsha Dawadi",
+      email: "dawadiavipsha01@gmail.com",
+      fb_link: "",
+      insta_link: "",
+      linkedin_link:
+        "https://www.linkedin.com/in/avipsha-dawadi-b3774327a?original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=np",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Community Leader",
+    },
+    {
+      name: "Zenitha Chalise",
+      email: "zenithachalise@gmail.com",
+      fb_link: "https://www.facebook.com/zen.itha.9/",
+      insta_link: "https://www.instagram.com/zenjtha/",
+      linkedin_link: "https://www.linkedin.com/in/zenitha-chalise-0610b3279/",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Community Leader",
+    },
+    {
+      name: "Abhinav Acharya",
+      email: "abhinavacharya486@gmail.com",
+      fb_link: "https://www.facebook.com/abhinav.acharya7s",
+      insta_link: "https://www.instagram.com/abhinav.bat/",
+      linkedin_link: "",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Community Leader",
+    },
 
-        {
-            "name": "Bibek Pulami",
-            "email": "bibekmm33@gmail.com",
-            "fb_link": "https://www.facebook.com/profile.php?id=100055852849657&mibextid=ZbWKwL",
-            "insta_link": "",
-            "linkedin_link": "",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Community Leader"
-        },
+    {
+      name: "Bibek Pulami",
+      email: "bibekmm33@gmail.com",
+      fb_link:
+        "https://www.facebook.com/profile.php?id=100055852849657&mibextid=ZbWKwL",
+      insta_link: "",
+      linkedin_link: "",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Community Leader",
+    },
 
-        {
-            "name": "Kumud Raj Ghimire",
-            "email": "ghimirekumudraj@gmail.com",
-            "fb_link": "https://m.facebook.com/profile.php/?id=100063454430616",
-            "insta_link": "",
-            "linkedin_link": "https://www.linkedin.com/mwlite/profile/in/kumudghimire/",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Community Leader"
-        },
-        {
-            "name": "Lenish Pandey",
-            "email": "lenishpandey@gmail.com",
-            "fb_link": "https://m.facebook.com/lenish.pandey",
-            "insta_link": "",
-            "linkedin_link": "https://www.linkedin.com/in/lenish-pandey-830720214",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Community Leader"
-        },
-        {
-            "name": "Gaurav Subedi",
-            "email": "thegauravsubedi@gmail.com",
-            "fb_link": "https://www.facebook.com/gaurav.subedi.5245?mibextid=ZbWKwL",
-            "insta_link": "https://instagram.com/gaurav.subedi0710?igshid=OGQ5ZDc2ODk2ZA==",
-            "linkedin_link": "",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Community Leader"
-        },
-        {
-            "name": "Sujal Bhattarai",
-            "email": "infinitelyfinite712@gmail.com",
-            "fb_link": "",
-            "insta_link": "@sssujalll",
-            "linkedin_link": "",
-            "image": "/images/Members/Saroj_Regmi.jpg",
-            "post": "Community Leader"
-        }
-    ]
+    {
+      name: "Kumud Raj Ghimire",
+      email: "ghimirekumudraj@gmail.com",
+      fb_link: "https://m.facebook.com/profile.php/?id=100063454430616",
+      insta_link: "",
+      linkedin_link: "https://www.linkedin.com/mwlite/profile/in/kumudghimire/",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Community Leader",
+    },
+    {
+      name: "Lenish Pandey",
+      email: "lenishpandey@gmail.com",
+      fb_link: "https://m.facebook.com/lenish.pandey",
+      insta_link: "",
+      linkedin_link: "https://www.linkedin.com/in/lenish-pandey-830720214",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Community Leader",
+    },
+    {
+      name: "Gaurav Subedi",
+      email: "thegauravsubedi@gmail.com",
+      fb_link: "https://www.facebook.com/gaurav.subedi.5245?mibextid=ZbWKwL",
+      insta_link:
+        "https://instagram.com/gaurav.subedi0710?igshid=OGQ5ZDc2ODk2ZA==",
+      linkedin_link: "",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Community Leader",
+    },
+    {
+      name: "Sujal Bhattarai",
+      email: "infinitelyfinite712@gmail.com",
+      fb_link: "",
+      insta_link: "@sssujalll",
+      linkedin_link: "",
+      image: "/images/Members/Saroj_Regmi.jpg",
+      post: "Community Leader",
+    },
+  ];
 
-    return data;
+  return data;
 }
