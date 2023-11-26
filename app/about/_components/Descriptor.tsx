d34ccea1a0f66737d06548ea90cadc11279782c2
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-26 18:24:33 +0545

    Changeable Descriptor

--- app/about/_components/Descriptor.tsx
+++ app/about/_components/Descriptor.tsx
@@ -1,13 +1,18 @@
 import type { TdescriptorData } from "../type"
 
-export default function Descriptor({ title, description, list = [] }: TdescriptorData) {
+export default function Descriptor({ title, descriptions = [], list = [] }: TdescriptorData) {
     return (
         <div className="descriptor flex space-y-small flex-col">
             <div className="title font-bold text-sub-title text-black-dark  ">
                 {title}
             </div>
-            <div className="description font-normal text-para text-black-mid">
-                {description}
+            <div className="description font-normal  space-y-small text-para text-black-mid">
+                {
+                    descriptions.map((description, index) => {
+                        return <div key={index}> {description}</div>
+                    })
+
+                }
                 <div className="list_con mt-small">
                     {
                         list?.map((li, index) => {
