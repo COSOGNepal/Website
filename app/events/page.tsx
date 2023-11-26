c598464973e85451f5d73da662a9f318e22717cc
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-26 12:45:28 +0545

    Updated the color and overall design of the slider.

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -8,17 +8,17 @@ export default function EventsPage() {
                 <Event />
                 <Event />
             </div>
-            <div className="slider_container max-w-[400px] h-[80vh] p-standard flex flex-col justify-between items-center ">
+            <div className="slider_container max-w-[400px] h-[80vh] p-standard flex flex-col justify-between items-center text-sub-para font-normal text-black-mid">
                 <div className="start_time">
                     17<sup>th </sup> Jan 2021
                 </div>
                 <div className="slider relative h-[90%]">
-                    <div className="line h-full w-[2px] bg-black-light"></div>
+                    <div className="line h-full w-[2px] bg-gray-dark"></div>
                     <div className="active_container h-max w-full">
-                        <div className="line_active h-[50px] w-[10px] bg-blue absolute top-0 left-[-3px] rounded-[4px]"></div>
+                        <div className="line_active h-[50px] w-[10px] bg-primary absolute top-0 left-[-3px] rounded-[4px]"></div>
                         <div className="flex w-max space-x-small absolute top-[40px] left-[-7.95px]">
-                            <div className="ball w-[20px] h-[20px] rounded-[10px] bg-blue"></div>
-                            <div className="current_date">
+                            <div className="ball w-[20px] h-[20px] rounded-[10px] bg-primary"></div>
+                            <div className="current_date font-bold">
                                 12<sup>th </sup> Dec 2022
                             </div>
                         </div>
