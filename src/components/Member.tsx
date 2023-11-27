255370dd762b236ca25329d8ab3868c4d932ecf3
sarojregmi200 <sarojregmi.official@gmail.com>
2023-11-27 13:54:00 +0545

    Fixed inconstitent behaviour accross every other devices.

--- src/components/Member.tsx
+++ src/components/Member.tsx
@@ -4,7 +4,7 @@ import type { Tmember } from "../../app/about/type";
 export default function Member({ data }: { data: Tmember }) {
     const { name, post, fb_link, insta_link, linkedin_link, image } = data;
     return (
-        <div className="member max-h-[350px] max-w-[320px] rounded-[5px] relative overflow-hidden group transition-all duration-100 ease-in-out">
+        <div className="member h-[350px] max-w-[320px] rounded-[5px] relative overflow-hidden group transition-all duration-100 ease-in-out">
             <Image src={image} height={350} width={320} alt={name}
                 className="image object-cover min-h-full" />
             <div className="description h-full w-full bg-gradient-to-t from-[rgba(0,0,0,0.7)] group-hover:from-[rgba(0,0,0,0.9)] to-transparent absolute top-0 left-0 p-standard align-baseline">
