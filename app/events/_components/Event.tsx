94c09f62220a893c036db2ad67c7e7db339786f4
Aashish Panthi <aashishpanthi11@gmail.com>
2023-11-28 20:13:41 +0545

    Render real data

--- app/events/_components/Event.tsx
+++ app/events/_components/Event.tsx
@@ -40,13 +40,13 @@ export default function Event({ data, index, states, activeBarHeightPerEvent }: 
         <div className="main_container max-w-[680px] h-max flex flex-col border-white-light border-2" ref={main_container}>
             <div className="imagesContainer grid grid-cols-2 grid-rows-2-250 gap-small w-full">
                 <div className="image col-span-2 overflow-hidden relative group cursor-pointer">
-                    <Image src="/images/Event.png" height={260} width={680} alt="event" className="event w-full h-max absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] group-hover:scale-105  transition-all ease-in-out duration-150 " />
+                    <Image src={images[0]} height={260} width={680} alt="event" className="event w-full h-max absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] group-hover:scale-105  transition-all ease-in-out duration-150 " />
                 </div>
                 <div className="image group overflow-hidden cursor-pointer h-max">
-                    <Image src="/images/Event.png" height={260} width={680} alt="event" className="image group-hover:scale-105  transition-all ease-in-out duration-150" />
+                    <Image src={images[1]} height={260} width={680} alt="event" className="image group-hover:scale-105  transition-all ease-in-out duration-150" />
                 </div>
                 <div className="image overflow-hidden group cursor-pointer h-max">
-                    <Image src="/images/Event.png" height={260} width={680} alt="event" className="image group-hover:scale-105  transition-all ease-in-out duration-150" />
+                    <Image src={images[2]} height={260} width={680} alt="event" className="image group-hover:scale-105  transition-all ease-in-out duration-150" />
                 </div>
             </div >
             <div className="desc_container h-auto w-full p-standard">
--- app/events/getEvents.tsx
+++ app/events/getEvents.tsx
@@ -1,43 +1,10 @@
 import type { Tevent } from "./type";
-export default async function getEvents(): Promise<Tevent[]> {
-    const events: Tevent[] = [
-        {
-            title: "Event title, is great",
-            date: "18th Jun 2020",
-            images: ["/images/Event.png", "/images/Event.png", "/images/Event.png"],
-            descriptions: ["The event description is very importnant as it explains the event and this can be very long as well as very short as well.",
-                "The event description is very importnant as it explains the event and this can be very long as well as very short as well.",
-                "The event description is very importnant as it explains the event and this can be very long as well as very short as well."
-            ]
-        },
-        {
-            title: "Event title, is great",
-            date: "19th Jun 2020",
-            images: ["/images/Event.png", "/images/Event.png", "/images/Event.png"],
-            descriptions: ["The event description is very importnant as it explains the event and this can be very long as well as very short as well.",
-                "The event description is very importnant as it explains the event and this can be very long as well as very short as well.",
-                "The event description is very importnant as it explains the event and this can be very long as well as very short as well."
-            ]
-        },
-        {
-            title: "Event title, is great",
-            date: "20th Jun 2020",
-            images: ["/images/Event.png", "/images/Event.png", "/images/Event.png"],
-            descriptions: ["The event description is very importnant as it explains the event and this can be very long as well as very short as well.",
-                "The event description is very importnant as it explains the event and this can be very long as well as very short as well.",
-                "The event description is very importnant as it explains the event and this can be very long as well as very short as well."
-            ]
-        },
-        {
-            title: "Event title, is great",
-            date: "21th Jun 2020",
-            images: ["/images/Event.png", "/images/Event.png", "/images/Event.png"],
-            descriptions: ["The event description is very importnant as it explains the event and this can be very long as well as very short as well.",
-                "The event description is very importnant as it explains the event and this can be very long as well as very short as well.",
-                "The event description is very importnant as it explains the event and this can be very long as well as very short as well."
-            ]
-        }
+import allevents from "@/data/events.json"
 
-    ]
+export default async function getEvents(): Promise<Tevent[]> {
+    const events: Tevent[] = allevents.map(event => ({
+        ...event,
+        descriptions: [event.description]
+    }));
     return events;
 }
