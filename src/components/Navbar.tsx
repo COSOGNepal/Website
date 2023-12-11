a156e31ff3d1cdc5728afb3db424cb31d3c327d9
Saroj Regmi <sarojregmi.official@gmail.com>
2023-12-11 09:59:19 +0545

    Fixed the full screen navigation.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="fixed hamcontainer z-50 p-standard  bg-[rgba(255,255,255,0.8)] backdrop-blur-[20px] m-standard rounded-full">
+      <div className="fixed hamcontainer z-50 p-standard  bg-[rgba(255,255,255,0.8)] backdrop-blur-[20px] m-standard rounded-full md:hidden">
         <div className="relative w-full h-full cursor-pointer"
             onClick={() => {
               setNavActive(!navActive);
@@ -26,15 +26,32 @@ const Navbar = () => {
       <nav
         className={`fixed inset-0 p-0 min-w-screen min-h-screen z-40 ${
           !navActive ? "pointer-events-none" : "pointer-events-auto"
-        }`}
+        }
+        `}
       >
         <main
           className={`${
             !navActive ? "-translate-y-full " : "translate-y-0"
-          } relative w-full h-full grid place-content-center duration-500 transition `}
+          } relative w-full h-full grid place-content-center duration-500 transition 
+            md:w-full md:h-[100px] md:translate-y-0
+            border-bottom-black border-bottom-2  
+          `}
         >
-          <div className="backdrop-blur-[20px] absolute inset-0 -z-10 bg-[rgba(255,255,255,0.5)]"></div>
-          <ul className="text-sub-title font-small flex items-center flex-col gap-3 text-faded uppercase">
+          <div className={`
+                backdrop-blur-[20px] absolute inset-0 -z-10 bg-[rgba(255,255,255,0.5)]
+                md:bg-[rgba(255,255,255,0.8)] 
+          `}></div>
+          <Image 
+            src={"/assets/logo.png"}
+            height={50}
+            width={50}
+            className={"absolute hidden md:block left-block top-[25px] "}
+            alt={"nav logo"}
+          />
+          <ul className={`
+                text-sub-title font-small flex items-center flex-col gap-3 text-faded uppercase
+                md:flex-row md:space-x-small md:items-center md:h-full md:max-w-[600px] md:text-sub-para md:margin-l-auto md:absolute md:right-block
+          `}>
             <Link href="/">
               <li
                 onClick={() => setNavActive(!navActive)}
