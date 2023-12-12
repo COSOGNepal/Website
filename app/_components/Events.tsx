8e6a4aac14338e7dfcd2dd55c0bf8ad1a386b680
Saroj Regmi <sarojregmi.official@gmail.com>
2023-12-12 10:27:10 +0545

    Made events section two row responsive.

--- app/_components/Events.tsx
+++ app/_components/Events.tsx
@@ -3,37 +3,36 @@ import React from "react";
 import getEvents from "../events/getEvents"
 
 const Events = async () => {
-    const EventsData = await getEvents({count: 4});
-    console.log(EventsData)
+    const EventsData = await getEvents();
     return (
         <main className="px-block w-screen mx-auto space-y-block">
             <SectionTitle title="Events" viewall="/events" />
             {/* events listing */}
-            <section className="w-full mx-auto grid grid-cols-1 sm:grid-cols-2 px-2 md:px-20 gap-4 bg-contain ">
+            <section className="w-full grid grid-cols-auto-fit-600 grid-rows-2-400 gap-small justify-center">
                 {EventsData.map((event, index) => (
                     <div
                         key={index}
                         style={{ backgroundImage: `url("${event.images[0]}")` }}
-                        className={`relative aspect-[12/9] rounded-xl overflow-hidden bg-no-repeat bg-cover`}
+                        className={`relative rounded-xl overflow-hidden bg-no-repeat bg-cover`}
                     >
-                        <div className="absolute  text-white px-8 pb-2 md:pb-6 bottom-0 h-1/2 w-full bg-gradient-to-t from-dark-transparent to-transparent flex justify-end flex-col gap-3">
+                        <div className="absolute text-white px-8 pb-2 md:pb-6 bottom-0 h-1/2 w-full bg-gradient-to-t from-dark-transparent to-transparent flex justify-end flex-col gap-3">
                             <span className="text-sm md:text-base font-light">
                                 {event.date}
                             </span>
                             <div>
-                            <h1 className="text-xl md:text-3xl font-bold">{event.title}</h1>
-                            <div className="descriptions"> 
-                            {
-                            event.descriptions.map((description, index)=>{
-                                    return (<p className="text-sm md:text-md font-medium" key={index}>
-                                            {description}
+                                <h1 className="text-xl md:text-3xl font-bold">{event.title}</h1>
+                                <div className="descriptions">
+                                    {
+                                        event.descriptions.map((description, index) => {
+                                            return (<p className="text-sm md:text-md font-medium" key={index}>
+                                                {description}
                                             </p>
-                                           )
-                                    })
-                            }
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                 </div>
                     </div>
                 ))}
             </section>
--- app/events/getEvents.tsx
+++ app/events/getEvents.tsx
@@ -2,20 +2,20 @@ import type { Tevent } from "./type";
 
 import eventsData from "@/data/events.json"
 
-export default async function getEvents(data:{count?: number} = {count: 0}): Promise<Tevent[]> {
+export default async function getEvents(data: { count?: number } = { count: 0 }): Promise<Tevent[]> {
     try {
         const count = data.count;
         if (!eventsData) throw new Error("Invalid json file");
         if (eventsData.length < 1) throw new Error("Json file cannot be empty");
-        
-        if(!count)
+
+        if (!count)
             return eventsData.reverse();
 
         const shortedEvents = eventsData
-                                .reverse()
-                                .filter((event, index) => {
-                                    return (index + 1) <= count 
-                                    })
+            .reverse()
+            .filter((_, index) => {
+                return (index + 1) <= count
+            })
         return shortedEvents
     }
     catch (e) {
