e56c74561a4c1f31de7a77e0555907e992bb3ba1
Saroj Regmi <sarojregmi.official@gmail.com>
2023-12-12 12:59:57 +0545

    Shifting of content on hover long description bug fixed

--- app/_components/Events.tsx
+++ app/_components/Events.tsx
@@ -15,12 +15,13 @@ const Events = async () => {
                         style={{ backgroundImage: `url("${event.images[0]}")` }}
                         className={`relative rounded-[5px] overflow-hidden bg-no-repeat bg-cover group`}
                     >
-                        <div className="absolute text-white px-8 pb-2 md:pb-6 bottom-0  w-full bg-gradient-to-t from-[rgba(0,0,0,0.8)] group-hover:from-30% flex justify-end flex-col"> <span className="text-info font-medium">
-                            {event.date}
-                        </span>
+                        <div className="absolute text-white px-8 pb-2 md:pb-6 bottom-0 h-1/2 w-full bg-gradient-to-t from-[rgba(0,0,0,0.8)] group-hover:from-30% flex justify-end flex-col">
+                            <span className="text-info font-medium">
+                                {event.date}
+                            </span>
                             <div className="mb-[-10px] group-hover:mb-[0] transition-all ease-in-out duration-250">
                                 <h1 className="text-title font-bold">{event.title}</h1>
-                                <div className="descriptions space-y-v-small hidden mt-[200px] group-hover:block group-hover:mt-[0px]">
+                                <div className="descriptions space-y-v-small opacity-0 absolute top-[200px] group-hover:opacity-100 group-hover:top-[0px] group-hover:static">
                                     {
                                         event.descriptions.map((description, index) => {
                                             return (<p className="text-sub-para font-medium" key={index}>
