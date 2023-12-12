dddd132b859149ba29b3cfda30ad6c47a880687e
Aashish Panthi <aashishpanthi11@gmail.com>
2023-12-12 15:08:36 +0545

    full stop; fr

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -105,7 +105,7 @@ const ContactPage = () => {
                             target="_blank"
                         >
                             Gmail
-                        </a>{" ."}
+                        </a>.
                     </p>
                 </div>
 
