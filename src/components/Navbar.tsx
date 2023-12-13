3b700546d4aeafbf5581f298a2b373bf164601d0
Saroj Regmi <sarojregmi.official@gmail.com>
2023-12-13 15:06:49 +0545

    Fixed the nav for bigger screen.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,6 @@ import React, { useState } from "react";
 
 const Navbar = () => {
     const [navActive, setNavActive] = useState(false);
-
     return (
         <>
             <div className="fixed hamcontainer z-50 p-standard  bg-[rgba(255,255,255,0.8)] backdrop-blur-[20px] m-standard rounded-full md:hidden">
@@ -24,11 +23,14 @@ const Navbar = () => {
                 </div>
             </div>
             <nav
-                className={`fixed left-0 overflow-hidden w-screen  min-h-screen md:min-h-[100px] h-[100px] z-40 md:pointer-events-auto${!navActive ? "pointer-events-none" : "pointer-events-auto"}`}
+                className={`fixed left-0 overflow-hidden min-h-screen md:min-h-[100px] h-[100px] z-40 md:pointer-events-auto
+                ${!navActive ? "pointer-events-none" : "pointer-events-auto"}
+                w-screen`}
             >
                 <main
                     className={`${!navActive ? "-translate-y-full " : "translate-y-0"
-                        } relative w-full h-full grid place-content-center duration-500 transition md:w-full md:h-[100px] md:translate-y-0 border-bottom-black border-bottom-2  `}
+                        } relative w-full h-full grid place-content-center duration-500 transition md:w-full md:h-[100px]
+                        md:translate-y-0 border-bottom-black border-bottom-2  `}
                 >
                     <div className={`
                     backdrop-blur-[20px] absolute inset-0 -z-10 bg-[rgba(255,255,255,0.5)]
@@ -39,13 +41,14 @@ const Navbar = () => {
                             src={"/assets/logo.png"}
                             height={50}
                             width={50}
-                            className={"absolute hidden md:block left-block top-[25px] "}
+                            className={"absolute hidden md:block left-block top-[25px]  brk-1400:left-[calc((100%-1400px)/2)]"}
                             alt={"nav logo"}
                         />
                     </Link>
                     <ul className={`
-                    text-sub-title font-small flex items-center flex-col gap-3 text-faded uppercase
-                        md:flex-row md:space-x-small md:items-center md:h-full md:max-w-[600px] md:text-sub-para md:margin-l-auto md:absolute md:right-block
+                        text-sub-title font-small flex items-center flex-col gap-3 text-faded uppercase
+                        md:flex-row md:space-x-small md:items-center md:h-full md:max-w-[600px] md:text-sub-para md:margin-l-auto
+                        md:absolute md:right-block brk-1400:right-[calc((100%-1400px)/2)]
                         `}>
                         <Link href="/">
                             <li
