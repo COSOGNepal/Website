56221398ec3b288936711a9a4cb201bd46fd8714
Saroj Regmi <sarojregmi.official@gmail.com>
2023-12-15 12:43:09 +0545

    Made the SectionTitle more responsive

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -8,10 +8,10 @@ type TSectionTitleProps = {
 export default function SectionTitle({ title, viewall }: TSectionTitleProps) {
     return (
         <div className="title_container max-w-[1400px] brk-1400:mx-auto flex justify-between items-center">
-            <h1 className="font-bold text-sub-title text-black-mid uppercase">
+            <h1 className="font-bold sm:text-sub-title text-para text-black-mid uppercase">
                 {title}
             </h1>
-            {viewall && <Link href={viewall} className="text-blue font-medium text-para uppercase"> View all</Link> || ""}
+            {viewall && <Link href={viewall} className="text-blue text-sub-para font-medium sm:text-para uppercase"> View all</Link> || ""}
         </div >
     )
 }
