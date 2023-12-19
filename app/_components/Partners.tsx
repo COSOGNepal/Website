0ff8297a29d7c87b3ee08a48c13afbe0c55c6922
Saroj Regmi <sarojregmi.official@gmail.com>
2023-12-19 19:01:03 +0545

    Made the partners section scrollable.

--- app/_components/Partners.tsx
+++ app/_components/Partners.tsx
@@ -1,6 +1,9 @@
+"use client"
 import SectionTitle from "@/components/SectionTitle";
 import Image from "next/image";
 import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 const partenersData = [
     "AWS Cloud Club",
@@ -10,29 +13,34 @@ const partenersData = [
     "Hackademedia",
     "Sagarmatha Hacktoberfest"
 ]
+const slidesPerView = Math.round(Math.min(innerWidth, 1400) / 192) - 1
 
 const Partners = () => {
     return (
         <main className="bg-gray-bg p-block space-y-block px-standard md:px-block brk-1400:px-0">
             <SectionTitle title="we've partnered with" />
             <section className="flex space-x-standard justify-center max-w-[1400px] mx-auto overflow-hidden">
-                {
-                    partenersData.map((partnerName, index) =>
-                        <div className={`w-48 h-48 aspect-square relative grid place-items-center bg-white rounded-md`} key={index}>
-                            <Image
-                                src={`/images/Partners/${partnerName
-                                    .split(" ")
-                                    .map(token => token.toLowerCase())
-                                    .join("_")}.png`
-                                }
-                                height={150}
-                                width={150}
-                                alt={partnerName}
-                            />
-                        </div>
-                    )
+                <Swiper spaceBetween={10} autoplay slidesPerView={slidesPerView}>
+                    {
+                        partenersData.map((partnerName, index) =>
+                            <SwiperSlide key={index}>
+                                <div className={`w-48 h-48 aspect-square relative grid place-items-center bg-white rounded-md`}>
+                                    <Image
+                                        src={`/images/Partners/${partnerName
+                                            .split(" ")
+                                            .map(token => token.toLowerCase())
+                                            .join("_")}.png`
+                                        }
+                                        height={150}
+                                        width={150}
+                                        alt={partnerName}
+                                    />
+                                </div>
+                            </SwiperSlide>
+                        )
 
-                }
+                    }
+                </Swiper>
             </section>
         </main>
     );
