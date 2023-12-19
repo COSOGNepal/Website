db8c213d28e75b109ce931e2ded0d85aebd459fd
Saroj Regmi <sarojregmi.official@gmail.com>
2023-12-19 17:16:01 +0545

    Improved the Partners rendering

--- app/_components/Partners.tsx
+++ app/_components/Partners.tsx
@@ -2,59 +2,36 @@ import SectionTitle from "@/components/SectionTitle";
 import Image from "next/image";
 import React from "react";
 
+const partenersData = [
+    "AWS Cloud Club",
+    "Code for Change",
+    "iTech Karnali",
+    "Sagarmatha Techfest",
+    "Hackademedia",
+    "Sagarmatha Hacktoberfest"
+]
+
 const Partners = () => {
     return (
         <main className="bg-gray-bg p-block space-y-block px-standard md:px-block brk-1400:px-0">
             <SectionTitle title="we've partnered with" />
             <section className="flex gap-4 justify-center max-w-[1400px] mx-auto">
-                <div className="w-48">
-                    <Image
-                        src={"/images/Partners/codeforchange.png"}
-                        width={130}
-                        height={130}
-                        alt="Code for Change"
-                    />
-                </div>
-                <div className="w-48">
-                    <Image
-                        src={"/images/Partners/aws_club.png"}
-                        width={130}
-                        height={130}
-                        alt="AWS Cloud Club"
-                    />
-                </div>
-                <div className="w-48">
-                    <Image
-                        src={"/images/Partners/itechkarnali.png"}
-                        width={130}
-                        height={130}
-                        alt="iTech Karnali"
-                    />
-                </div>
-                <div className="w-48">
-                    <Image
-                        src={"/images/Partners/sagarmatha_techfest.png"}
-                        width={130}
-                        height={130}
-                        alt="Sagarmatha Techfest"
-                    />
-                </div>
-                <div className="w-48">
-                    <Image
-                        src={"/images/Partners/sagarmatha_hacktoberfest.png"}
-                        width={130}
-                        height={130}
-                        alt="Sagarmantha Hacktoberfest"
-                    />
-                </div>
-                <div className="w-48">
-                    <Image
-                        src={"/images/Partners/hackademedia.png"}
-                        width={130}
-                        height={130}
-                        alt="Hackademedia"
-                    />
-                </div>
+                {
+                    partenersData.map((partnerName, index) =>
+                        <div className="w-48 h-48 aspect-square relative" key={index}>
+                            <Image
+                                src={`/images/Partners/${partnerName
+                                    .split(" ")
+                                    .map(token => token.toLowerCase())
+                                    .join("_")}.png`
+                                }
+                                fill
+                                alt="Code for Change"
+                            />
+                        </div>
+                    )
+
+                }
             </section>
         </main>
     );
