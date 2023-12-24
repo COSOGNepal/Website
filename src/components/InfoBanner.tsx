1526922986b71bb85fb935b0f0a9c7a1034d5aaa
Saroj Regmi <sarojregmi.official@gmail.com>
2023-12-24 12:57:56 +0545

    Made the InfoBanner responsive

--- src/components/InfoBanner.tsx
+++ src/components/InfoBanner.tsx
@@ -12,7 +12,7 @@ const InfoBanner = ({
 }) => {
     return (
         <main className="h-fit text-white bg-primary bg-[url('/assets/bgPattern.svg')] bg-cover bg-fixed bg-blend-multiply mt-section -mb-sectionw-full py-block brk-1400:px-[calc(((100%-1400px)/2)+50px)] px-standard">
-            <div className="max-w-[1400px] flex justify-between items-center ">
+            <div className="max-w-[1400px] flex justify-between md:items-center flex-col  md:flex-row space-y-block items-start">
                 <div className="text-white flex justify-start">
                     {leftContent}
                 </div>
