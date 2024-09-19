4c674dfb14c43a4c123ed0f6619fe4badf2c3808
Saroj <sarojregmi.official@gmail.com>
2024-09-19 23:57:45 +0545

    feat(Events): Add support for unmaged data.
    Now the `json` file can contain data in any order till the date
    is in `do MMMM YYYY` "eg: 16th August 2024" format it will get sorted according to the date

--- src/lib/sortings.ts
+++ src/lib/sortings.ts
@@ -4,25 +4,29 @@ const isObject = <T>(value: T): boolean => {
     return typeof value === "object" && value !== null && !Array.isArray(value);
 }
 
+// NOTE: works only on dates array or array of objects with a key as date.
 const quickSort = <T>(a: T[], sortKey?: string): T[] => {
-    let pivot = a[0];
-
-    if (sortKey && isObject(pivot))
-        pivot = (a[0] as Record<string, any>)[sortKey];
     if (a.length <= 1)
         return a
+    let pivot = a[0];
+    if (sortKey && isObject(pivot))
+        pivot = (a[0] as Record<string, any>)[sortKey];
 
     const leftArray = [];
     const rightArray = [];
 
+
     for (let i = 1; i < a.length; i++) {
-        if (a[i] < pivot || sortKey && (a[i] as Record<string, any>)?.[sortKey] < pivot) {
+        if (
+            (!isObject(a[i]) && new Date(a[i] as string | Date) < new Date(pivot as string | Date))
+            || (sortKey && new Date((a[i] as Record<string, any>)?.[sortKey]) < new Date(pivot as string | Date))
+        ) {
             leftArray.push(a[i]);
             continue;
         }
         rightArray.push(a[i])
     }
-    return [...quickSort(leftArray), pivot, ...quickSort(rightArray)];
+    return [...quickSort(leftArray, sortKey), a[0], ...quickSort(rightArray, sortKey)];
 }
 
 const dataSchema = z.array(z.record(z.any())) || z.array(z.date());
@@ -34,15 +38,16 @@ const quickSortByDateParamsSchema = z.object({
 })
 
 
+type quickSortByDataReturnType<T> = T extends undefined ? Date[] : T[];
+
 // accepts both array of date and a record containing a feild as date and returns a sorted one.
-export const quickSortByDate = (params: z.infer<typeof quickSortByDateParamsSchema>) => {
-    const isArray = Array.isArray(params.data);
-    const sortedData = isArray && params.sortKey
-        ? quickSort<Date>(params.data as Date[])
-        : quickSort<Record<string, any>>(params.data, params.sortKey);
+export const quickSortByDate = <T>(params: z.infer<typeof quickSortByDateParamsSchema>): quickSortByDataReturnType<T> => {
+    const sortedData = isObject(params.data[0])
+        ? quickSort<Record<string, any>>(params.data, params.sortKey)
+        : quickSort<Date>(params.data as Date[]);
 
     return params.direction === "Asc"
-        ? sortedData
-        : sortedData.toReversed();
+        ? sortedData as quickSortByDataReturnType<T>
+        : sortedData.toReversed() as quickSortByDataReturnType<T>;
 }
 
