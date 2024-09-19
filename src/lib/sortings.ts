73804253da172069e3d52d83eb6b6fa35d1f13f3
Saroj <sarojregmi.official@gmail.com>
2024-09-19 18:47:17 +0545

    feat(dataStructure): quick sort implemented

--- src/lib/sortings.ts
+++ src/lib/sortings.ts
@@ -1,21 +1,48 @@
-import { isDate } from "util/types";
-import { z, ZodSchema } from "zod";
+import { z } from "zod";
 
-type quickSortByDateParams = {
-    direction: "Asc" | "Des"
-} & ({ array: any[] | Date[], sortKey: string, } | { array: Date[] })
+const isObject = <T>(value: T): boolean => {
+    return typeof value === "object" && value !== null && !Array.isArray(value);
+}
+
+const quickSort = <T>(a: T[], sortKey?: string): T[] => {
+    let pivot = a[0];
+
+    if (sortKey && isObject(pivot))
+        pivot = (a[0] as Record<string, any>)[sortKey];
+    if (a.length <= 1)
+        return a
+
+    const leftArray = [];
+    const rightArray = [];
+
+    for (let i = 1; i < a.length; i++) {
+        if (a[i] < pivot || sortKey && (a[i] as Record<string, any>)?.[sortKey] < pivot) {
+            leftArray.push(a[i]);
+            continue;
+        }
+        rightArray.push(a[i])
+    }
+    return [...quickSort(leftArray), pivot, ...quickSort(rightArray)];
+}
+
+const dataSchema = z.array(z.record(z.any())) || z.array(z.date());
 
 const quickSortByDateParamsSchema = z.object({
+    data: dataSchema,
     direction: z.enum(["Asc", "Des"]).default("Des"),
-    array: z.array(z.any() || Date),
     sortKey: z.string().optional(),
-}).refine(data => {
-    if (data.sortKey)
-        return data.array.every((date: any | Date) => isDate(date));
-});
+})
 
 
-export const quickSortByDate = (params: typeof quickSortByDateParamsSchema) => {
+// accepts both array of date and a record containing a feild as date and returns a sorted one.
+export const quickSortByDate = (params: z.infer<typeof quickSortByDateParamsSchema>) => {
+    const isArray = Array.isArray(params.data);
+    const sortedData = isArray && params.sortKey
+        ? quickSort<Date>(params.data as Date[])
+        : quickSort<Record<string, any>>(params.data, params.sortKey);
 
-
+    return params.direction === "Asc"
+        ? sortedData
+        : sortedData.toReversed();
 }
+
